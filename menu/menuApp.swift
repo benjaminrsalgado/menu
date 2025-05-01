d41e1059f7f33b2d2bd2232b10d52d83f5093791
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-05-01 12:40:52 -0600

    Initial Commit

---
+++ menu/menuApp.swift
@@ -0,0 +1,17 @@
+//
+//  menuApp.swift
+//  menu
+//
+//  Created by Benjamin Rojo on 01/05/25.
+//
+
+import SwiftUI
+
+@main
+struct menuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
