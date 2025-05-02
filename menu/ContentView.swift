f1ce9506e9abc6af993753af8f95e6f1212436aa
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-05-02 10:21:48 -0600

    i add the list of the menu and sf symbols

--- menu/ContentView.swift
+++ menu/ContentView.swift
@@ -22,15 +22,57 @@ struct ContentView: View {
                     .background(Color.brown.opacity(0.8))
                     .padding()
                 
-                Text("This is the best restaurant ")
+                Text("This is the best restaurant in New York City")
+                    .multilineTextAlignment(.center)
                     .font(.title)
                     .foregroundColor(.white)
                     .background(Color.brown.opacity(0.8))
                     .padding()
             
+                HStack{
+                 
+                    Text("Pasta🍝")
+                        .foregroundColor(.white)
+                        .padding(.top)
+                    Text("Pizza 🍕")
+                        .foregroundColor(.white)
+                        .padding(.top)
+                    Text("Hamburgesa 🍔")
+                        .foregroundColor(.white)
+                        .padding(.top)
+                    Text("Sushi🍣")
+                        .foregroundColor(.white)
+                        .padding(.top)
+                  
+                      
+                }
+                HStack{
+                    Button(action: {
+                        print("Ordered")
+                    }) {
+                        Label("Order", systemImage: "fork.knife")
+                            .foregroundColor(.green)
+                            .font(.system(size: 24, weight: .bold))
+                            .padding()
+                            .background(Color.white.opacity(0.2))
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
+                    }
+                    Button(action: {
+                        print("Favorte")
+                    }) {
+                        Label("Order", systemImage: "star.fill")
+                            .foregroundColor(.yellow)
+                            .font(.system(size: 23, weight: .bold))
+                            .padding()
+                            .background(Color.white.opacity(0.2))
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
+                    }
+                }
             }
+   
         }
-    
     }
 }
 
